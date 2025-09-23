96fec9ed42d8eb49f0c702e938b2ef28aa86d6fd
arunkapila <arunj.kapila@gmail.com>
2025-09-24 00:40:44 +0100

    Initial Commit

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace NoWingsDip
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "NoWingsDip";
+        public const string PLUGIN_NAME = "NoWingsDip";
+        public const string PLUGIN_VERSION = "1.0.0.0";
+    }
+}
