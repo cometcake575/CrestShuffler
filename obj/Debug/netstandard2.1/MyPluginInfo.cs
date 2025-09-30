4024ebfb18772daf6c66ce67095925f0f0f92189
arunkapila <arunj.kapila@gmail.com>
2025-10-01 00:17:50 +0100

    Initial Commit

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -1,9 +1,9 @@
-namespace HeartlessBellhome
+namespace CrestShuffler
 {
     public static class MyPluginInfo
     {
-        public const string PLUGIN_GUID = "HeartlessBellhome";
-        public const string PLUGIN_NAME = "HeartlessBellhome";
+        public const string PLUGIN_GUID = "CrestShuffler";
+        public const string PLUGIN_NAME = "CrestShuffler";
         public const string PLUGIN_VERSION = "1.0.0.0";
     }
 }
